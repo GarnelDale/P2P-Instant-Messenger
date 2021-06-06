f2369e95395c3bd8c2bcc1f316ae3d7064930095
Torin Bolander <bol19001@byui.edu>
2021-06-05 23:35:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Overview
+This is a very basic instant messaging application that can be used to communicate over text between two computers on a local area network. It is peer to peer where one user will 'host' the connection by running the host program and the other user will run the client program and input the host's local IP. After the connection is established, text messages can be sent back and forth between the machines until one or the other quit the program.
+
+I wrote this program to learn the basics of socket programming and GUI creation in Python.
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running (you will need to show two pieces of software running and communicating with each other) and a walkthrough of the code.}
+
+[Software Demo Video](https://youtu.be/pcFTHB9mjWk)
+
+# Network Communication
+This program utilizes a peer-to-peer architecture for a local area network.
+
+The data transfer protocol used is TCP running on port 5580.
+
+The messages that can be sent are text-only messages. 
+
+# Development Environment
+Visual Studio Code
+Python 3.9.5 64bit
+Socket library
+tkinter library
+
+# Useful Websites
+* [Python Reference Library: Socket](https://docs.python.org/3.6/library/socket.html#other-functions)
+* [Tk Tutorial](https://tkdocs.com/tutorial/index.html)
+* [Python Socket Basics](https://www.tutorialspoint.com/python/python_networking.htm)
+
+
+# Future Work
+* Improve the send/receive loop to not slow down the process as much.
+* Style and layout the Tk window to look more professional.
+* Update the client program to have the host ip prompt and input be handled in the GUI.
+* Clean up the quit conditions to not be so abrupt on the side who closed second.
---
+++ p2pHost.py
@@ -0,0 +1,88 @@
+# Import needed libraries
+import socket
+from tkinter import *
+from tkinter import ttk
+
+# Open hosting socket 
+s = socket.socket()
+name = socket.gethostname()
+print(name)
+port = 5580
+s.bind((name,port))
+
+# Create the window
+root = Tk()
+root.title("P2P Host")
+root.geometry("300x300")
+
+# Put a grid frame inside to manage everything
+mainframe = ttk.Frame(root)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(0, weight=1)
+
+# mysend method sends until the buffer is empty
+def mysend(*args):
+    # Extract message from input box and encode as binary for
+    # sending
+    msg = text.get().encode()
+    # Quit condition
+    if msg.decode() == 'quit':
+        c.close()
+        root.destroy()
+    # Take length so we know how much to send
+    MSGLEN = len(msg)
+    totalsent = 0
+    # Start sending
+    while totalsent < MSGLEN:
+        sent = c.send(msg[totalsent:])
+        # If message is done, stop sending
+        if sent == 0:
+            break
+        # Update total
+        totalsent = totalsent + sent
+    # Clear the input box
+    text.set("")
+
+# myreceive is an idle receive that takes in any incoming data,
+# decodes it to a string, and updates the screen to show it.
+def myreceive(self):
+    try:
+        global receive
+        receive.set(c.recv(1024).decode())
+    except socket.timeout:
+        pass
+    self.after(500, myreceive, self)
+
+# Text entry box creation
+text = StringVar()
+text_entry = ttk.Entry(mainframe, width=20, textvariable=text)
+text_entry.grid(column=0, row=1, sticky=(W, S))
+
+# Incoming text message 
+receive = StringVar()
+ttk.Label(mainframe, textvariable=receive, wraplength=200).grid(column=0, row=0, sticky=(W, E))
+
+# Send button 
+ttk.Button(mainframe, text="Send", command=mysend).grid(column=1, row=1, sticky=(W,E))
+
+# Listen for and connect to client socket
+s.listen(1)
+c, addr = s.accept()
+print (f'Got connection from {addr}')
+c.settimeout(.5)
+
+# Add a little space between widgets
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+# Place cursor in the text entry and bind Enter key to the send as well
+text_entry.focus()
+root.bind("<Return>", mysend)
+
+# Start idle receive loop
+root.after_idle(myreceive, root)
+
+# Start window loop
+root.mainloop()
